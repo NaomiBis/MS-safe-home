616be27a2616ff9ae5d5821c853e8f1bc4ecfa9f
NaomiBis <68124814+NaomiBis@users.noreply.github.com>
2020-10-18 00:55:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -26,90 +26,23 @@ $response .= "2. Safe House \n";
 $response .= "3. Leaving your abuser \n";
 $response .= "4. Document Storage\n";
 }
-//Menu for a user who selects '1' from the third menu above
-// Will be brought to this third menu screen
-else if ($text == "1*1*1") {
-$response .= "This is only for immediate emergency \n";
-$responce .= "Press 1 to continue and 0 to cancel \n";
-;
-}
-else if ($text == "1*1*1*1") {
-$response = "Help is on the way, stay strong";
-}
-else if ($text == "1*1*0") {
-$response = "END Emergency alert has been canceled";
-}
-// Menu for a user who selects "2" from the second menu above
-// Will be brought to this fourth menu screen
-else if ($text == "2") {
-$response = "CON Safe houses are organisations that can help you with a accomodation and other services \n";
-$response .= "Please Enter 1 to confirm your need for a safe house 0 to cancel\n";
-}
-// Menu for a user who selects "1" from the fourth menu screen
-else if ($text == "1*2*1") {
-$response = "CON A safe house near you will contact you soon \n";
-$response .= "Please enter your phone number /n";
-}
-else if ($text == "1*2*1*0") {
-$response = "END You have canceled the request /n";
-}
-// Menu for a user who selects "3" from the second menu above
-// Will be brought to this fith menu screen
-else if ($text == "1*3") {
-$response = "CON Do you want steps on how to leave your abuser? \n";
-$response .= "Please Enter 1 to confirm 0 to cancel\n";
-}
-// Menu for a user who enters "1" from the fifth menu
-else if ($text == "1*3*1") {
-$response = "CON An organisation will contact you to come up with a safety plan that is best for you";
-$response .= "Enter your phone number to continue \n";
-$response .= "Enter 0 to cancel \n";
-}
-else if ($text == "1*3*1*1") {
-$response = "END Thank you, you will be contacted soon to discuss a safety plan";
-}
-else if ($text == "1*3*1*0") {
-$response = "END Your request has been canceled";
-}
-// Menu for a user who enters "4" from the second menu above
-// Will be brought to this sixth menu screen
-else if ($text == "1*4") {
-$response = "CON This is for keepimg your important documents kept safely at an organisation\n";
-$response .= "Please Enter 1 to continue \n";
-}
-// Menu for a user who enters "1" from the sixth menu
-else if ($text == "1*4*1") {
-$response = "Please take the following original doccuments";
-$response .= "ID/Birth certificate";
-$response .= "Educational certificates";
-$response .= "Legal doccuments ";
-$response .= " Medical doccuments";
-$response .= " Take these doccuments to an organisation near you \n";
+
+   //Menu for a user who selects '1' from the third menu above
+  // Will be brought to this third menu screen
+  else if ($text == "1*1*1") {
+  $response .= "This is only for immediate emergency \n";
+  $response .= "Enter 1 to continue \n";
+  $response .= "Enter 0 to cancel";
+  }
+
+  else if ($text == "1*1*1*1") {
+  $response = "Your alert has been recieved";
+  $response .= "Enter your address below";
+  $response .= "Our emergency rescue team is on the way";
 }
 
-// Menu for a user who enters "1" from the fifth menu
-else if ($text == "1*3*1") {
-$response = "CON An organisation will contact you to come up with a safety plan that is best for you";
-$response .= "Enter your phone number to continue \n";
-$response .= "Enter 0 to cancel \n";
+
+  else if ($text == "1*1*1*0") {
+  $response = "Your request has been canceled";
+  $response .= "Thanks for using our service, \nBrought to you by Imbokodo";
 }
-else if ($text == "1*3*1*1") {
-$response = "END Thank you, you will be contacted soon to discuss a safety plan";
-}
-else if ($text == "1*3*1*0") {
-$response = "END Your request has been canceled";
-}
-// Menu for a user who enters "4" from the second menu above
-// Will be brought to this sixth menu screen
-else if ($text == "1*1*4") {
-$response = "Please take the following original doccuments";
-$response .= "ID/Birth certificate";
-$response .= "Educational certificates";
-$response .= "Legal doccuments ";
-$response .= " Medical doccuments";
-$response .= " Take these doccuments to an organisation near you \n";
-}
-//echo response
-header('Content-type: text/plain');
-echo $response
-?>
